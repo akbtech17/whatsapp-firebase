b0f1457f51f11a22cb8fbcd6a76390105afc88f1
anshul bansal <akb.tech17@gmail.com>
2020-10-07 20:23:11 +0530

    added google login to our loginpage

--- src/login.js
+++ src/login.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './login.css'
 import {Button} from "@material-ui/core"
+import { auth, provider } from './firebase'
 
 
 
 function login() {
 
     const signIn = () => {
-
-    }
+        auth.signInWithPopup(provider)
+        .then((result) => console.log(result))
+        .catch((error) => alert(error.message));
+    };
 
     return (
         <div className="login">
